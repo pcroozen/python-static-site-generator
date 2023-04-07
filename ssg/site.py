56ed2ae7f307ebb928299fd9094cbee90ed281a5
Patrick Roozen <patrick.roozen@gmail.com>
2023-04-07 18:17:03 +0200

    First commit Pluralsight project

---
+++ ssg/site.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source: str, dest: str) -> None:
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path: Path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
