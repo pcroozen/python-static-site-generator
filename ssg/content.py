9795b043807f8644c82f1b3ccc003e4267a3d0fc
Patrick Roozen <patrick.roozen@gmail.com>
2023-04-12 20:56:25 +0200

    section 7 done

--- ssg/content.py
+++ ssg/content.py
@@ -5,11 +5,28 @@ from yaml import FullLoader, load
 
 
 class Content(Mapping):
+    def __init__(self, metadata, content) -> None:
+        self.data = metadata
+        self.data["content"] = content
+        super().__init__()
+
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
-        cls(metadata, content)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
